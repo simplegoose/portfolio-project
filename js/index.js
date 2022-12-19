6d2bb298d5a5b5cc9ce4ef573c60b5562d176754
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2022-12-19 12:45:23 -0500

    Working in the close button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.flex-nav');
+const closeBtn = document.querySelector('.close');
+const linksMenu = nav.getElementsByTagName('a');
 
 menuBtn.addEventListener('click', () => {
   nav.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('open');
 });
